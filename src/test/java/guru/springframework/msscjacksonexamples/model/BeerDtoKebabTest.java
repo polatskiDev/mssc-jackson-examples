d796d4a8016513e2393aac6228684431841a5759
o.polat <o.polat@tech-lab.io>
2023-02-09 16:19:12 +0100

    Json Testing Naming strategies -kebab

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("kebab")
+@JsonTest
+public class BeerDtoKebabTest extends BaseTest{
+
+
+    @Test
+    void testKebab() throws JsonProcessingException {
+        BeerDto dto = getDto();
+
+        String json = objectMapper.writeValueAsString(dto);
+
+        System.out.println(json);
+    }
+}
