792064e73fb6963247bcf4bb64fbd9c30982e1b6
o.polat <o.polat@tech-lab.io>
2023-02-09 15:34:26 +0100

    Json Testing Naming strategies

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoSnakeTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("snake")
+@JsonTest
+public class BeerDtoSnakeTest extends BaseTest{
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testSnake() throws JsonProcessingException {
+        BeerDto beerDto = getDto();
+        String json = objectMapper.writeValueAsString(beerDto);
+        System.out.println(json);
+    }
+
+}
